3135a104571c8f8e1bd2cf4e701e86c65c3db6b4
gagewillette <gagew0513@gmail.com>
2022-03-27 16:54:51 -0400

    udpate

--- lib/homeContent.dart
+++ lib/homeContent.dart
@@ -1,16 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class homeContent extends StatefulWidget
-{
+import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
+
+class homeContent extends StatefulWidget {
+  const homeContent({Key? key}) : super(key: key);
+
   @override
-  State<homeContent> createState() => _homeContent();
+  State<homeContent> createState() => _HomeContent();
 }
 
-class _homeContent extends State<homeContent>
-{
+class _HomeContent extends State<homeContent> {
+  WeatherFactory wf = WeatherFactory("7f57497d63748a8dad533be2d3b6e23c",
+      language: Language.ENGLISH);
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home")));
+
+
+
+    return ListView(
+      children: [
+        Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.fromLTRB(0, 100, 0, 30),
+            child: Text(
+              "70°",
+              style: const TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 50),
+            )),
+        Container(
+            alignment: Alignment.topCenter,
+            child: Text("68° / 80°" ,
+                style: const TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 30)))
+      ],
+    );
   }
-  
 }
